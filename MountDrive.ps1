4f03c5a2bd3a488d9413ea4dcb8729966fb646c3
Simon Sawert <simon@sawert.se>
2019-08-17 12:43:46 +0200

    Fix whitespaces

--- MountDrive.ps1
+++ MountDrive.ps1
@@ -1,14 +1,14 @@
-For ( $i=3; $i -gt 0; $i-- ) {
+For ( $i = 3; $i -gt 0; $i-- ) {
     $error.clear()
     
     $MappedDrives = Get-SmbMapping | where -property Status -Value Unavailable -EQ | select LocalPath,RemotePath
 
-    if ( @($MappedDrives).Length -lt 1) {
+    if ( @($MappedDrives).Length -lt 1 ) {
         Write-Host "No unavailable network drivers to mount, exiting"
         break
     }
 
-    foreach( $MappedDrive in $MappedDrives) {
+    foreach( $MappedDrive in $MappedDrives ) {
         try {
             New-SmbMapping -LocalPath $MappedDrive.LocalPath -RemotePath $MappedDrive.RemotePath -Persistent $True
         }
